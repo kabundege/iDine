53b9c2db1c1ddd851320691fd45b8ca9adec6576
kabundege <christophekwizea1@gmail.com>
2023-05-30 18:05:07 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by < Bible  /> on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
